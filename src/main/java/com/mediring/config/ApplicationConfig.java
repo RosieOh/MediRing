5305a73ed01bffffe953988ade883a7ffd085912
RosieOh <20172207@gm.ac.kr>
2023-11-18 02:58:52 +0900

    🏢feat : Configuration 완🏢

---
+++ src/main/java/com/mediring/config/ApplicationConfig.java
@@ -0,0 +1,4 @@
+package com.mediring.config;
+
+public class ApplicationConfig {
+}
---
+++ src/main/java/com/mediring/domain/member.java
@@ -0,0 +1,20 @@
+package com.mediring.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class member {
+    private String id;
+    private String pw;
+    private String name;
+    private String email;
+    private String tel;
+    private String addr1;
+    private String addr2;
+    private String postcode;
+    private String regdate;
+    private int pt;
+    private int visited;
+}
